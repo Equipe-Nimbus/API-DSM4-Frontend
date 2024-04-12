b438e05d2f0494361e34d32e176d1fe15ff6105a
HenningerJv <josevictor.henninger@gmail.com>
2024-04-11 22:31:50 -0300

    colocando algumas alteracoes na navbar

--- src/navBar.tsx
+++ src/navBar.tsx
@@ -10,9 +10,10 @@ import { RiErrorWarningLine } from "react-icons/ri";
 import { RiUserLine } from "react-icons/ri";
 import { RiFileList3Line } from "react-icons/ri";
 import { RiSettings4Line } from "react-icons/ri";
+import { AiOutlineCloud } from "react-icons/ai";
 import Link from "next/link";
 
-function navBar() {
+export default function navBar() {
     const [opcoesMobile, setOpcoesMobile] = useState(false)
     const router = useRouter();
     const toggleOpcoesMobile = () => {
@@ -22,6 +23,11 @@ function navBar() {
         <>
             <aside className={`bg-bg-100 p-2 ${opcoesMobile ? "h-screen block top-0 duration-300 z-50 w-full" : "h-16 duration-300 w-full"}`}>
                 <div className="cursor-pointer" onClick={() => toggleOpcoesMobile()}>
+                <ul className="my-10 flex flex-col gap-5">
+                        <li className="flex cursor-pointer flex-row items-center gap-1 p-1.5 text-2xl font-medium text-on-bg22 duration-200 hover:rounded hover:bg-hover-bg22 xl:text-xl">
+                            <AiOutlineCloud />
+                        </li>
+                    </ul>
                 <ul className="my-10 flex flex-col gap-5">
                         <li className="flex cursor-pointer flex-row items-center gap-1 p-1.5 text-2xl font-medium text-on-bg22 duration-200 hover:rounded hover:bg-hover-bg22 xl:text-xl">
                             <RiDashboardFill />
