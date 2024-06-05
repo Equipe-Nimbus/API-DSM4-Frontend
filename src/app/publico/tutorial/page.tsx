72570bbc7cfc95c73ebe64ed1d4f15624dde826b
HenningerJv <josevictor.henninger@gmail.com>
2024-06-05 10:15:03 -0300

    adicionando as imagens alinhadas ao texto

--- src/app/publico/tutorial/page.tsx
+++ src/app/publico/tutorial/page.tsx
@@ -22,52 +22,70 @@ export default function Tutorial() {
                 <h2 className="text-text-on-background text-base font-medium">Sensores Climáticos:</h2>
             </div>
             <div className="bg-bg-100 p-4 rounded-md drop-shadow">
-                <p className="text-text-on-background text-base font-medium justify-between">
-                    Temperatura:<Image src="/sensor-temperatura.webp" alt="station icon" width={99} height={44} />
+            <Image src="/sensor-temperatura.webp" alt="station icon" width={99} height={44} /><p className="text-text-on-background text-base font-medium justify-between ml-32">Temperatura:
                     <br />
                     Um sensor de temperatura é um dispositivo que mede a temperatura do ambiente ou
-                    de um objeto e converte essa informação em
+                    de um objeto e 
+                    <br />
+                    converte essa informação em
                     dados que podem ser utilizados para monitoramento, controle ou registro.
                 </p>
-                <br />
+                <br /><br />
                 <p className="text-text-on-background text-base font-medium justify-between">Pressão:
                     <br />
                     Um sensor de pressão, também conhecido como barômetro, é um dispositivo usado para
-                    medir a pressão atmosférica. A pressão atmosférica é a força exercida pelo peso da coluna de ar acima de um
-                    ponto específico na superfície da Terra. Medir essa pressão é crucial em meteorologia
-                    porque ela influencia e é influenciada por vários fenômenos climáticos.
-                </p><Image src="/barometro.jpg" alt="station icon" width={99} height={44} />
-                <br />
-                <p className="text-text-on-background text-base font-medium justify-between">Umidade: <Image src="/sensor-umidade.jpg" alt="station icon" width={99} height={44} />
+                    medir a pressão atmosférica. 
+                    <br />
+                    A pressão atmosférica é a força exercida pelo peso da coluna de ar acima de um
+                    ponto específico na superfície da Terra. 
+                    <br />
+                    Medir essa pressão é crucial em meteorologia
+                    porque ela influencia e é influenciada por vários fenômenos climáticos. <Image src="/barometro.jpg" alt="station icon" width={99} height={45} className="ml-auto"/>
+                </p>
+                <br /><br />
+                <Image src="/sensor-umidade.jpg" alt="station icon" width={99} height={44} /><p className="text-text-on-background text-base font-medium justify-between ml-24">Umidade:
                     <br />
                     Um higometro ou sensor de umidade é um instrumento usado para medir a umidade do ar,
-                    a medida é normalmente feita a partir da corrente elétrica de minerais que
-                    mudam sua voltagem de acordo com a umidade do ar, mas também existem métodos
+                    a medida é normalmente 
+                    <br />
+                    feita a partir da corrente elétrica de minerais que
+                    mudam sua voltagem de acordo com a umidade do ar, 
+                    <br />
+                    mas também existem métodos
                     usando cabelo ou termometros, essa medição é importante para caracterizar um clima local, prever chuvas e
-                    outros fenomenos, além de poder prever incêndios.
+                    outros fenomenos, além de poder prever incêndios. 
                 </p>
-                <br />
+                <br /><br />
                 <p className="text-text-on-background text-base font-medium justify-between">Velocidade do Vento:
                     <br />
                     Um anêmometro ou sensor de velocidade do vento é uma ferramente para medir a velocidade dos ventos,
-                    normalmente medida pela velocidade de rotação de hélices, mas também existe a medição por temperatura de fios
+                    <br />
+                    normalmente medida pela velocidade de rotação de hélices, mas também existe 
+                    a medição por temperatura de fios
                     e pulsos ultrassônicos, essa medição é importante para prever e indentificar mudanças e padrões que podem influenciar a
-                    formação de outros fenomenos meterológicos. <Image src="/sensor-velocidade-vento.webp" alt="station icon" width={99} height={44} />
+                    formação de outros fenomenos meterológicos. <Image src="/sensor-velocidade-vento.webp" alt="station icon" width={79} height={44} className="ml-auto" />
                 </p>
                 <br />
-                <p className="text-text-on-background text-base font-medium justify-between">Direção do Vento:<Image src="/sensor-direcao-vento.jpg" alt="station icon" width={99} height={44} />
+                <Image src="/sensor-direcao-vento.jpg" alt="station icon" width={99} height={44} /><p className="text-text-on-background text-base font-medium justify-between ml-32">Direção do Vento:
                     <br />
                     Os sensores de direção de vento medem a movimentação do ar em relação a pontos de referência,
-                    como pontos cardeias, são importantes para entender como o vento influencia as condições locais e regionais,
+                    como pontos cardeias, 
+                    <br />
+                    são importantes para entender como o vento influencia as condições locais e regionais,
                     como frentes frias e tempestades.
                 </p>
-                <br />
+                <br /><br />
                 <p className="text-text-on-background text-base font-medium justify-between">Pluviometro:
                     <br />
                     Um pluviometro basculante é um instrumento metereológico projetado para medir a quantidade de
-                    precipitação de um lugar em determinado tempo, consiste de um funil, um mecânismo e um sensor que
-                    verifica o movimento do mecânismo, esses dados são extremamente importantes pois ajudam a entender
-                    padrões climaticos e prever eventos extremos e monitorar recursos hidricos. <Image src="/pluviometro.webp" alt="station icon" width={99} height={44} />
+                    <br />
+                    precipitação de um lugar em determinado tempo, consiste de um funil, 
+                    <br />
+                    um mecânismo e um sensor que
+                    verifica o movimento do mecânismo, 
+                    <br />
+                    esses dados são extremamente importantes pois ajudam a entender
+                    padrões climaticos e prever eventos extremos e monitorar recursos hidricos. <Image src="/pluviometro.webp" alt="station icon" width={99} height={44} className="ml-auto" />
                 </p>
                 <br />
                 <p className="text-text-on-background text-base font-medium justify-between">A importância de cada sensor:
